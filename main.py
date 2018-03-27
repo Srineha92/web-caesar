dad9976e23057b0932ebc04f239e377ff65bbf21
srineha27 <indukuri.neha@gmail.com>
2018-03-26 19:28:50 -0500

    Finish Web caesar

--- main.py
+++ main.py
@@ -4,50 +4,61 @@ from caesar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
+page_header = """
 <!DOCTYPE html>
-
 <html>
     <head>
+        <title>FlickList</title>
+    </head>
+    <body>
+        <h1>FlickList</h1>
+"""
+
+page_footer = """
+    </body>
+</html>
+"""
+
+
+form = """
+    <head>
         <style>
-            form {
+        form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
        <form method='post'>
        Rotate by: 
        <input type="text" name="rot" id="rot"  value="0"/> <br>
-       <textarea  rows = "200" cols = "400" name="text" > </textarea><br>
+       <textarea  name="text">{0} </textarea><br>
        <input type="submit" value="Submit Query"/>
        </form>
     </body>
-</html>
-
-"""
+</html>  """
 @app.route("/", methods=['POST'])
 def encrypt():
     text_encrypt = request.form['text']
-    
-    rotation = rotate_string(text_encrypt)
-    web_encrypt_element = "<h1>" + rotation + "</h1>"
+    rot_encrypt = int(request.form['rot'])
 
-    return web_encrypt_element
+    rotation = rotate_string(text_encrypt,rot_encrypt)
+    content =  rotation 
+    return form.format(content)
+
 
 @app.route("/")
 def index():
-    encrypted_mesg = encrypt()
-    return encrypted_mesg
+    return form.format("")
 
 app.run()    
